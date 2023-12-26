85e4362931333fa525ddd5cf957c63773feb172d
Jesper Eskilson <jesper.eskilson@klarna.com>
2023-12-26 09:40:55 +0100

    cleanup

--- utils.pl
+++ utils.pl
@@ -1,19 +1,4 @@
-:- module(utils, [file_line/2,
-                  write_to_file/2]).
-
-file_line(File, Line) :-
-    setup_call_cleanup(open(File, read, In),
-                       stream_line(In, Line),
-                       close(In)).
-
-stream_line(In, Line) :-
-    repeat,
-    (  read_line_to_string(In, Line0),
-       Line0 \== end_of_file
-    -> Line0 = Line
-    ;  !,
-       fail
-    ).
+:- module(utils, [sum/2]).
 
 sum(Xs, Sum) :-
     sum(Xs, Sum, 0).
@@ -22,8 +7,3 @@ sum([], Sum, Sum).
 sum([X|Xs], Sum, Acc) :-
     Acc0 is X + Acc,
     sum(Xs, Sum, Acc0).
-
-write_to_file(Data, Filename) :-
-    setup_call_cleanup(open(Filename, write, Stream),
-                       write(Stream, Data),
-                       close(Stream)).
