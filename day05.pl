6a578f79c4e4780cb953cf4836bf18d2a49409a6
Jesper Eskilson <jesper.eskilson@klarna.com>
2023-12-31 18:18:24 +0100

    day 5 example work

--- day05.pl
+++ day05.pl
@@ -55,28 +55,23 @@ example_data([[[50, 98, 2],
                [56, 93, 4]]]).
 
 ex1(Location) :-
+    example_data(Maps),
+
     (Seed #= 79) #\/ (Seed #= 14) #\/ (Seed #= 55) #\/ (Seed #= 13),
     Location in 0..99,
 
-    example_data(Maps),
     apply_maps(Seed, Maps, Location),
 
     % Find the smallest location possible
     once(labeling([min(Location)], [Seed, Location])).
 
-ex2(Location) :-
-    Seed1 in 79..92,
-    Seed2 in 55..67,
-    Location in 0..99,
-    Location1 in 0..99,
-    Location2 in 0..99,
-
+ex2(Seed-Location) :-
     example_data(Maps),
-    apply_maps(Seed1, Maps, Location1),
-    apply_maps(Seed2, Maps, Location2),
 
-    (Location1 #< Location2) #==> (Location #= Location1),
-    (Location1 #>= Location2) #==> (Location #= Location2),
+    (Seed in 79..92) #\/ (Seed in 55..67),
+    Location in 0..99,
 
-    % Find the smallest location possible
-    labeling([min(Location)], [Seed1, Seed2, Location1, Location2, Location]).
+    apply_maps(Seed, Maps, Location),
+
+    %% % Find the smallest location possible
+    labeling([min(Location)], [Seed, Location]).
