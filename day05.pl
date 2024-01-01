9ddf440e91829cd3ca96be1dbaeeda6235b859c0
Jesper Eskilson <jesper.eskilson@klarna.com>
2024-01-01 10:25:46 +0100

    tweaks

--- day05.pl
+++ day05.pl
@@ -5,6 +5,14 @@
                library(pcre),
                library(clpfd)]).
 
+%! apply_range_rules(+X, +Ranges, -Y, -Conds, -LS)
+%
+% Apply FD constraints for a list of [DstStart,SrcStart,Len] range
+% mappings. X is the input intervals to be mapped represented as a
+% single FD variable. Y is the output interval. Conds is unified with
+% a list of reified 1/0 variables representing each of the
+% conditions. This is used to be able to apply the default condition
+% if none of the rules apply.
 apply_range_rules(_, [], _, [], LS) :-
     empty_fdset(LS).
 apply_range_rules(X, [Range|Ranges], Y, [Cond|Conds], LSOut) :-
@@ -33,12 +41,12 @@ solve({part1, Location}) :-
     parse1(Seed, Maps),
     apply_maps(Seed, Maps, Location, LocSet),
     Location in_set LocSet,
-    once(labeling([min(Location)], [Seed, Location])).
+    once(labeling([ff, min(Location)], [Seed, Location])).
 solve({part2, Location}) :-
     parse2(Seed, Maps),
     apply_maps(Seed, Maps, Location, LocSet),
     Location in_set LocSet,
-    once(labeling([min(Location)], [Seed, Location])).
+    once(labeling([ff, min(Location)], [Seed, Location])).
 
 % Parser
 parse1(Seed, Maps) :-
